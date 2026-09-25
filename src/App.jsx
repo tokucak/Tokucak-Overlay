924a9c19a7e55570f2b567672e3c862fe7e0d5f7
tokucak <emre07koca@gmail.com>
2026-09-25 20:19:07 +0300

    Add RealtimeIRL map to overlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import Header from "./components/Header";
 
+const mapboxToken = import.meta.env.VITE_MAPBOX_TOKEN;
+
+const rtirlMapUrl =
+  `https://overlays.rtirl.com/leaflet.html` +
+  `?ak6fu9l6rf4x4kav=ak6fu9l6rf4x4kav` +
+  `&access_token=${mapboxToken}` +
+  `&style=mapbox/streets-v11` +
+  `&zoom=5` +
+  `&lang=en` +
+  `&attribution=0` +
+  `&indicatorStyle=eyJoZWlnaHQiOjEyLCJ3aWR0aCI6MTIsImJvcmRlclJhZGl1cyI6NTAsImJhY2tncm91bmRDb2xvciI6ImN5YW4ifQ%3D%3D`;
+
 function App() {
   return (
     <div className="overlay">
       <Header />
 
-      {/* Moblin Map Widget buraya gelecek */}
-      <div className="map"></div>
+      <div className="map">
+        <iframe
+          className="rtirlMap"
+          src={rtirlMapUrl}
+          title="RealtimeIRL GPS Map"
+          allow="geolocation"
+        />
+      </div>
     </div>
   );
 }
